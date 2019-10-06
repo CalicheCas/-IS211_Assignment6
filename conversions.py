af010ce18207caa242349876f5a888a17fdf8100
Carlo <carlogcasas@gmail.com>
2019-10-05 21:50:04 -0400

    Created 4 more functions with respective unit test. All 30 tests passing'

--- conversions.py
+++ conversions.py
@@ -9,3 +9,23 @@ def convertCelsiusToFahrenheit(self, celsius):
 
     f = (celsius * 1.8) + 32
     return round(f, 2)
+
+def convertFahrenheitToCelsius(self, fahrenheit):
+
+    c = (fahrenheit - 32) / 1.8
+    return round(c, 2)
+
+def convertFahrenheitToKelvin(self, fahrenheit):
+
+    k = (fahrenheit + 459.67) * (5/9)
+    return round(k, 2)
+
+def convertKelvinToCelsius(self, kelvin):
+
+    c = kelvin - 273.15
+    return round(c, 2)
+
+def convertKelvinToFahrenheit(self, kelvin):
+
+    f = (kelvin - 273.15) * (9/5) + 32
+    return round(f, 2)
--- test.py
+++ test.py
@@ -4,7 +4,13 @@ import conversions
 
 class Test(unittest.TestCase):
 
+    #############################################################
+    #                   CELSIUS TO KELVIN                       #
+    #############################################################
+
     def test_convertCelsiusToKelvin_when_celsius_is_0(self):
+
+        print('test_convertCelsiusToKelvin_when_celsius_is_0')
 
         # :given
         celsius = 0
@@ -18,6 +24,8 @@ class Test(unittest.TestCase):
 
     def test_convertCelsiusToKelvin_when_celsius_is_negative(self):
 
+        print('test_convertCelsiusToKelvin_when_celsius_is_negative')
+
         # :given
         celsius = -32
 
@@ -29,6 +37,8 @@ class Test(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_convertCelsiusToKelvin_when_celsius_is_positive(self):
+
+        print('test_convertCelsiusToKelvin_when_celsius_is_positive')
 
         # :given
         celsius = 32
@@ -42,6 +52,8 @@ class Test(unittest.TestCase):
 
     def test_convertCelsiusToKelvin_when_exception_raised(self):
 
+        print('test_convertCelsiusToKelvin_when_exception_raised')
+
         # :given
         celsius = 'someString'
 
@@ -50,6 +62,8 @@ class Test(unittest.TestCase):
             conversions.convertCelsiusToKelvin(self, celsius)
 
     def test_convertCelsiusToKelvin_when_fraction(self):
+
+        print('test_convertCelsiusToKelvin_when_fraction')
         # :given
         celsius = 33.21
 
@@ -60,7 +74,12 @@ class Test(unittest.TestCase):
         # :then
         self.assertEqual(expected, actual)
 
+    #############################################################
+    #               CELSIUS TO FAHRENHEIT                       #
+    #############################################################
+
     def test_convertCelsiusToFahrenheit_when_celsius_is_0(self):
+        print('test_convertCelsiusToFahrenheit_when_celsius_is_0')
 
         # :given
         celsius = 0
@@ -74,6 +93,7 @@ class Test(unittest.TestCase):
 
     def test_convertCelsiusToFahrenheit_when_celsius_is_negative(self):
 
+        print('test_convertCelsiusToFahrenheit_when_celsius_is_negative')
         # :given
         celsius = -30
 
@@ -85,6 +105,8 @@ class Test(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_convertCelsiusToFahrenheit_when_celsius_is_positive(self):
+
+        print('test_convertCelsiusToFahrenheit_when_celsius_is_positive')
 
         # :given
         celsius = 18
@@ -98,6 +120,8 @@ class Test(unittest.TestCase):
 
     def test_convertCelsiusToFahrenheit_when_exception_raised(self):
 
+        print('test_convertCelsiusToFahrenheit_when_exception_raised')
+
         # :given
         celsius = 'someString'
 
@@ -106,12 +130,293 @@ class Test(unittest.TestCase):
             conversions.convertCelsiusToFahrenheit(self, celsius)
 
     def test_convertCelsiusToFahrenheit_when_fraction(self):
+
+        print('test_convertCelsiusToFahrenheit_when_fraction')
+
         # :given
         celsius = 33.21
 
         # :when
         expected = 91.78
         actual = conversions.convertCelsiusToFahrenheit(self, celsius)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    #############################################################
+    #               FAHRENHEIT TO CELSIUS                       #
+    #############################################################
+
+    def test_convertFahrenheitToCelsius_when_fahrenheit_is_0(self):
+
+        print('test_convertFahrenheitToCelsius_when_fahrenheit_is_0')
+
+        # :given
+        fahrenheit = 0
+
+        # :when
+        expected = -17.78
+        actual = conversions.convertFahrenheitToCelsius(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertFahrenheitToCelsius_when_fahrenheit_is_negative(self):
+
+        print('test_convertFahrenheitToCelsius_when_fahrenheit_is_negative')
+
+        # :given
+        fahrenheit = -32
+
+        # :when
+        expected = -35.56
+        actual = conversions.convertFahrenheitToCelsius(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertFahrenheitToCelsius_when_fahrenheit_is_positive(self):
+
+        print('test_convertFahrenheitToCelsius_when_fahrenheit_is_positive')
+
+        # :given
+        fahrenheit = 32
+
+        # :when
+        expected = 0
+        actual = conversions.convertFahrenheitToCelsius(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertFahrenheitToCelsius_when_exception_raised(self):
+
+        print('test_convertFahrenheitToCelsius_when_exception_raised')
+
+        # :given
+        fahrenheit = 'someString'
+
+        # :when
+        with self.assertRaises(TypeError):
+            conversions.convertFahrenheitToCelsius(self, fahrenheit)
+
+    def test_convertFahrenheitToCelsius_when_fraction(self):
+
+        print('test_convertFahrenheitToCelsius_when_fraction')
+        # :given
+        fahrenheit = 33.21
+
+        # :when
+        expected = 0.67
+        actual = conversions.convertFahrenheitToCelsius(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    #############################################################
+    #               FAHRENHEIT TO KELVIN                        #
+    #############################################################
+
+    def test_convertFahrenheitToKelvin_when_fahrenheit_is_0(self):
+        print('test_convertFahrenheitToKelvin_when_fahrenheit_is_0')
+
+        # :given
+        fahrenheit = 0
+
+        # :when
+        expected = 255.37
+        actual = conversions.convertFahrenheitToKelvin(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertFahrenheitToKelvin_when_fahrenheit_is_negative(self):
+
+        print('test_convertFahrenheitToKelvin_when_fahrenheit_is_negative')
+        # :given
+        fahrenheit = -30
+
+        # :when
+        expected = 238.71
+        actual = conversions.convertFahrenheitToKelvin(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertFahrenheitToKelvin_when_fahrenheit_is_positive(self):
+
+        print('test_convertFahrenheitToKelvin_when_fahrenheit_is_positive')
+
+        # :given
+        fahrenheit = 18
+
+        # :when
+        expected = 265.37
+        actual = conversions.convertFahrenheitToKelvin(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertFahrenheitToKelvin_when_exception_raised(self):
+
+        print('test_convertFahrenheitToKelvin_when_exception_raised')
+
+        # :given
+        fahrenheit = 'someString'
+
+        # :when
+        with self.assertRaises(TypeError):
+            conversions.convertFahrenheitToKelvin(self, fahrenheit)
+
+    def test_convertFahrenheitToKelvin_when_fraction(self):
+
+        print('test_convertFahrenheitToKelvin_when_fraction')
+
+        # :given
+        fahrenheit = 33.21
+
+        # :when
+        expected = 273.82
+        actual = conversions.convertFahrenheitToKelvin(self, fahrenheit)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    #############################################################
+    #               KELVIN TO CELSIUS                           #
+    #############################################################
+
+    def test_convertKelvinToCelsius_when_celsius_is_0(self):
+
+        print('test_convertKelvinToCelsius_when_celsius_is_0')
+
+        # :given
+        kelvin = 0
+
+        # :when
+        expected = -273.15
+        actual = conversions.convertKelvinToCelsius(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertKelvinToCelsius_when_celsius_is_negative(self):
+
+        print('test_convertKelvinToCelsius_when_celsius_is_negative')
+
+        # :given
+        kelvin = -32
+
+        # :when
+        expected = -305.15
+        actual = conversions.convertKelvinToCelsius(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertKelvinToCelsius_when_celsius_is_positive(self):
+
+        print('test_convertKelvinToCelsius_when_celsius_is_positive')
+
+        # :given
+        kelvin = 32
+
+        # :when
+        expected = -241.15
+        actual = conversions.convertKelvinToCelsius(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertKelvinToCelsius_when_exception_raised(self):
+
+        print('test_convertKelvinToCelsius_when_exception_raised')
+
+        # :given
+        kelvin = 'someString'
+
+        # :when
+        with self.assertRaises(TypeError):
+            conversions.convertKelvinToCelsius(self, kelvin)
+
+    def test_convertKelvinToCelsius_when_fraction(self):
+
+        print('test_convertKelvinToCelsius_when_fraction')
+        # :given
+        kelvin = 33.21
+
+        # :when
+        expected = -239.94
+        actual = conversions.convertKelvinToCelsius(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    #############################################################
+    #               KELVIN  TO FAHRENHEIT                       #
+    #############################################################
+
+    def test_convertKelvinToFahrenheit_when_celsius_is_0(self):
+        print('test_convertKelvinToFahrenheit_when_celsius_is_0')
+
+        # :given
+        kelvin = 0
+
+        # :when
+        expected = -459.67
+        actual = conversions.convertKelvinToFahrenheit(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertKelvinToFahrenheit_when_celsius_is_negative(self):
+
+        print('test_convertKelvinToFahrenheit_when_celsius_is_negative')
+        # :given
+        kelvin = -30
+
+        # :when
+        expected = -513.67
+        actual = conversions.convertKelvinToFahrenheit(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertKelvinToFahrenheit_when_celsius_is_positive(self):
+
+        print('test_convertKelvinToFahrenheit_when_celsius_is_positive')
+
+        # :given
+        kelvin = 18
+
+        # :when
+        expected = -427.27
+        actual = conversions.convertKelvinToFahrenheit(self, kelvin)
+
+        # :then
+        self.assertEqual(expected, actual)
+
+    def test_convertKelvinToFahrenheit_when_exception_raised(self):
+
+        print('test_convertKelvinToFahrenheit_when_exception_raised')
+
+        # :given
+        kelvin = 'someString'
+
+        # :when
+        with self.assertRaises(TypeError):
+            conversions.convertKelvinToFahrenheit(self, kelvin)
+
+    def test_convertKelvinToFahrenheit_when_fraction(self):
+
+        print('test_convertKelvinToFahrenheit_when_fraction')
+
+        # :given
+        kelvin = 33.21
+
+        # :when
+        expected = -399.89
+        actual = conversions.convertKelvinToFahrenheit(self, kelvin)
 
         # :then
         self.assertEqual(expected, actual)
